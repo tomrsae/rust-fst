0f185b8d842823efd3b5de0ff46d2be3bccfd6b1
tomrsae <tommy.saetre@hotmail.com>
2024-04-25 14:47:04 +0100

    Renamed lib

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,43 @@
+use rsfsm_base::state::State;
+use rsfsm_proc::{ make_fsm, fsm_trigger };
+
 fn main() {
     println!("Hello, world!");
+    make_fsm!(
+        name: "Coin Machine",
+        states: [
+            Locked,
+            Unlocked
+        ],
+        triggers: [
+            push,
+            insert_coin
+        ]
+    );
+}
+
+#[fsm_trigger { Unlocked => Locked }]
+fn push() {
+
+}
+
+#[fsm_trigger { Locked => Unlocked }]
+fn insert_coin() {
+
+}
+
+struct Locked {
+
+}
+
+impl State for Locked {
+
+}
+
+struct Unlocked {
+
+}
+
+impl State for Unlocked {
+
 }
--- rsfsm_proc/src/lib.rs
+++ rsfsm_proc/src/lib.rs
@@ -10,6 +10,11 @@ pub fn make_fsm(tokens : TokenStream) -> TokenStream {
     make_fsm::impl_make_fsm(&ast)
 }
 
+#[proc_macro_attribute]
+pub fn fsm_trigger(attr: TokenStream, item: TokenStream) -> TokenStream {
+    panic!("oopsie")
+}
+
 fn parse_tokens(tokens: TokenStream) -> DeriveInput {
     syn::parse(tokens).unwrap()
 }
